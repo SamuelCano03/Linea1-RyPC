016df2e9963b8b14600cc39cb6f9e3f047809fed
SamuelCano03 <U202116508@upc.edu.pe>
2024-06-12 19:49:48 -0500

    base commit

---
+++ src/main/java/com/tren/linea1_service/Linea1ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tren.linea1_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Linea1ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Linea1ServiceApplication.class, args);
+	}
+
+}
